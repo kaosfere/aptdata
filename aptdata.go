4a82c48a2d5a82c6760ce15ee319bf107dadceea
Rob Jones <rcj@rcj.io>
2017-12-18 21:35:26 -0600

    simultaneous download working

--- aptdata.go
+++ aptdata.go
@@ -2,9 +2,12 @@ package aptdata
 
 import "github.com/coreos/bbolt"
 import _ "github.com/vmihailenco/msgpack"
-import "github.com/hashicorp/go-getter"
-import _ "fmt"
-import "net/url"
+import "fmt"
+import "os"
+import "net/http"
+import "io"
+import "errors"
+import "time"
 
 type AptDB struct {
 	boltDB *bolt.DB
@@ -48,16 +51,68 @@ type Runway struct {
 	End2Displaced int64
 }
 
+func downloadDataFile(dataDir string, filename string, url string, c chan error) {
+	fmt.Println("Downloading", filename)
+	fullPath := fmt.Sprintf("%s/%s", dataDir, filename)
+	out, err := os.Create(fullPath)
+	if err != nil {
+		c <- err
+		return
+	}
+	defer out.Close()
+
+	response, err := http.Get(url)
+	if err != nil {
+		c <- err
+		return
+	}
+	if response.StatusCode != 200 {
+		c <- errors.New(fmt.Sprintf("response code %d for %s", response.StatusCode, url))
+		out.Close()
+		os.Remove(fullPath)
+		return
+	}
+	defer response.Body.Close()
+
+	_, err = io.Copy(out, response.Body)
+	if err != nil {
+		c <- err
+		return
+	}
+
+	c <- nil
+}
+
 func OpenDB(path string) (db *AptDB, err error) {
 	var boltDB *bolt.DB
 	boltDB, err = bolt.Open(path, 0644, nil)
 	return &AptDB{boltDB: boltDB}, err
 }
 
-func DownloadData(dataDir string) error {
-	var u url.URL
-	u.Scheme = "http"
-	u.Host = "ourairports.com"
-	u.Path = "/data/airports.csv"
-	return getter.Get(dataDir, u)
+func DownloadData(dataDir string) (err error) {
+	files := [4]string{"airports.csv", "runways.csv", "countries.csv", "regions.csv"}
+	channels := make([]chan error, 4)
+
+	for i, file := range files {
+		c := make(chan error)
+		channels[i] = c
+		go downloadDataFile(dataDir, file, fmt.Sprintf("http://ourairports.com/data/%s", file), c)
+	}
+
+	numdone := 0
+	for numdone < len(files) {
+		for i, c := range channels {
+			select {
+			case err = <-c:
+				if err != nil {
+					return err
+				}
+				numdone += 1
+				//				fmt.Println("DID ONE", files[i]) // logging?
+			default:
+				time.Sleep(100 * time.Millisecond) // prevent spin-polling
+			}
+		}
+	}
+	return nil
 }
